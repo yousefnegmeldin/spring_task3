57144b9067bdf2a129f57854ca26fabb51833a85
yousefnegmeldin <yousefnegm@gmail.com>
2024-08-05 13:56:02 +0300

    chore: added more operations in swagger

--- src/main/java/com/example/demo/controller/RoomController.java
+++ src/main/java/com/example/demo/controller/RoomController.java
@@ -11,17 +11,23 @@ import com.example.demo.model.enums.ReservationStatus;
 import com.example.demo.model.enums.RoomType;
 import com.example.demo.service.ReservationService;
 import com.example.demo.service.RoomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/room")
+@Tag(name = "Room API", description = "API for room management")
 public class RoomController {
 
     private final RoomService roomService;
@@ -33,42 +39,78 @@ public class RoomController {
         this.reservationService = reservationService;
     }
 
+    @Operation(summary = "Create a room", description = "Create a new room")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Room created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid room data", content = @Content)
+    })
     @PostMapping
-    public ResponseEntity<?> createRoom(@RequestBody RoomDTO room){
+    public ResponseEntity<?> createRoom(@RequestBody RoomDTO room) {
         roomService.saveRoom(RoomMapper.toRoom(room));
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Get available rooms", description = "Get a list of available rooms")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of available rooms", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Room.class)))
+    })
     @GetMapping("/available")
-    public ResponseEntity<List<Room>> getAvailableRooms(){
+    public ResponseEntity<List<Room>> getAvailableRooms() {
         return ResponseEntity.ok().body(roomService.getReadyRooms());
     }
 
+    @Operation(summary = "Get rooms by type", description = "Get a list of rooms by type")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of rooms by type", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Room.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid room type", content = @Content)
+    })
     @GetMapping("/{type}")
-    public ResponseEntity<List<Room>> getRoomByFilter(@PathVariable RoomType type){
-        return ResponseEntity.ok().body(roomService.getRoomByType(type));
+    public ResponseEntity<List<RoomDTO>> getRoomByFilter(@PathVariable RoomType type) {
+        List<RoomDTO> roomsFound = roomService
+                .getRoomByType(type)
+                .stream()
+                .map(RoomMapper::toRoomDTO)
+                .toList();
+        return ResponseEntity.ok().body(roomsFound);
     }
 
-
+    @Operation(summary = "Reserve a room", description = "Book a room, the response contains the reservationDTO")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Room reserved successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationDTO.class))),
+            @ApiResponse(responseCode = "409", description = "Reservation already exists", content = @Content)
+    })
     @PostMapping("/book")
     public ResponseEntity<?> bookRoom(@RequestBody ReservationDTO reservationDTO) {
-        Reservation reservationToBook = reservationService.saveReservation(reservationDTO);
-        return ResponseEntity.ok().body(ReservationMapper.toReservationDTO(reservationToBook));
+        try {
+            Reservation reservationToBook = reservationService.saveReservation(reservationDTO);
+            return ResponseEntity.ok().body(ReservationMapper.toReservationDTO(reservationToBook));
+        } catch (ReservationAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
     }
 
-
+    @Operation(summary = "Cancel a reservation", description = "Cancel a reservation by room ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reservation cancelled successfully"),
+            @ApiResponse(responseCode = "404", description = "Reservation not found", content = @Content)
+    })
     @DeleteMapping("/{roomId}")
-    public ResponseEntity<?> cancelReservation(@PathVariable Long roomId){
+    public ResponseEntity<?> cancelReservation(@PathVariable Long roomId) {
         Optional<Reservation> reservation = reservationService.getReservationById(roomId);
-        if(reservation.isEmpty()){
-            return ResponseEntity.notFound().build();
+        if (reservation.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         reservationService.cancelReservation(reservation.get());
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Update a reservation", description = "Update a reservation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reservation updated successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid reservation data", content = @Content)
+    })
     @PutMapping
-    public ResponseEntity<ReservationDTO> updateReservation(@RequestBody ReservationDTO reservationDTO){
+    public ResponseEntity<ReservationDTO> updateReservation(@RequestBody ReservationDTO reservationDTO) {
         Reservation reservationToBook = reservationService.updateReservation(reservationDTO);
         return ResponseEntity.ok().body(ReservationMapper.toReservationDTO(reservationToBook));
     }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,9 +2,16 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.UserDTO;
 import com.example.demo.exceptions.UserNotFoundException;
+import com.example.demo.model.Room;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import com.example.demo.mapper.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +20,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User API", description = "API for user management")
 public class UserController {
 
     private final UserService userService;
@@ -29,7 +37,11 @@ public class UserController {
 
     }
 
-
+    @Operation(summary = "Get user by email", description = "Get user by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "user found by email", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid user type", content = @Content)
+    })
     @GetMapping("/{email}")
     public ResponseEntity<UserDTO> getUser(@PathVariable String email){
         User user = userService.getUserByEmail(email);
